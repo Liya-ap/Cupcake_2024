361fdd814835c846d6f6826f6b3676ebf0eb4979
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-04-02 11:05:09 +0200

    added basic classes and files without implementation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,18 @@
 package app;
 
+import app.config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing here:
     }
 }
