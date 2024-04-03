956c0df046ea3845e8ddd9d74818b20015821156
rebeccaburon <rebecca.mary.buron@hotmail.com>
2024-04-03 23:57:14 +0200

    Tilføjelse af Order, OrderDetails og OrderMapper

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,4 +1,13 @@
 package app.entities;
 
 public class User {
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
